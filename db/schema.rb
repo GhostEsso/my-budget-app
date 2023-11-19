a13f5c20742eef6c9404ad3adcd033c053296d09
GhostEsso <lilipitaham@gmail.com>
2023-11-19 15:00:58 +0000

    migrations generated to add reference column, FK and index for each table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_19_144837) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_19_145902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "group_purchases", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.bigint "purchase_id", null: false
+    t.index ["group_id"], name: "index_group_purchases_on_group_id"
+    t.index ["purchase_id"], name: "index_group_purchases_on_purchase_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -24,6 +28,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_19_144837) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "purchases", force: :cascade do |t|
@@ -31,6 +37,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_19_144837) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_purchases_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +47,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_19_144837) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "group_purchases", "groups", on_delete: :cascade
+  add_foreign_key "group_purchases", "purchases", on_delete: :cascade
+  add_foreign_key "groups", "users", on_delete: :cascade
+  add_foreign_key "purchases", "users", column: "author_id", on_delete: :cascade
 end
