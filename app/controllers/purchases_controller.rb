8a05fe307021421a3b12c2aad5309d2e1c8be55d
GhostEsso <lilipitaham@gmail.com>
2023-11-19 16:17:08 +0000

    nested routes created for groups and purchases

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,13 @@
 class PurchasesController < ApplicationController
-    def index; end
-  
-    def show; end
-  
-    def new; end
-  
-    def edit; end
-  
-    def update; end
-  
-    def delete; end
-  end
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def edit; end
+
+  def update; end
+
+  def delete; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "groups#index"
+  resources :groups do
+    resources :purchases
+  end
 end
