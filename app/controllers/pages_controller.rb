2ad102bd8a0bf69d835d9e15b6223e36b934c129
GhostEsso <lilipitaham@gmail.com>
2023-11-20 10:28:45 +0000

    show, edit & update actions created in Users Controller to allow users to account information

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
-  def splash
-  end
+  def splash; end
 
-  def about
-  end
+  def about; end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get splash" do
+  test 'should get splash' do
     get pages_splash_url
     assert_response :success
   end
 
-  test "should get about" do
+  test 'should get about' do
     get pages_about_url
     assert_response :success
   end
