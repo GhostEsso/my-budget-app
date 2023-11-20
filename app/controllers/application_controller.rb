6282c098ad5b1a77efba4cd66597d7d691eada79
GhostEsso <lilipitaham@gmail.com>
2023-11-20 10:07:34 +0000

    ApplicationController updated to permit allowed inputs and authenticate users before actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+    allowed_params = [:name, :email, :password, :password_confirmation, :current_password]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: allowed_params)
+    devise_parameter_sanitizer.permit(:sign_in, keys: allowed_params.first(2))
+    devise_parameter_sanitizer.permit(:account_update, keys: allowed_params)
+    end
 end
