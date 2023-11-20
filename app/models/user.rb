4fa55312590ddbc5c80bcfde559bd868242a4bc1
GhostEsso <lilipitaham@gmail.com>
2023-11-20 08:50:38 +0000

    notice and alert added to application.html.erb IO to see error/success message, :confirmable added to devise in userModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :purchases, foreign_key: 'author_id', dependent: :destroy
   has_many :groups, dependent: :destroy
 
