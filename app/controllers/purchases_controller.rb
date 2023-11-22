64e4ea36e2893380f13a4de7513248ae5b3b313f
GhostEsso <lilipitaham@gmail.com>
2023-11-22 08:56:38 +0000

    create action updated to perform redirections and show error/success flash messages

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,42 +9,51 @@ class PurchasesController < ApplicationController
   end
 
   def new
-    @groups = current_user.groups
     @purchase = Purchase.new
-    # Création d'un nombre initial de group_purchases associés à la purchase
-    # La quantité dépend du nombre de groupes de l'utilisateur actuel
-    @groups.size.times { @purchase.group_purchases.build }
   end
 
   def create
-    @purchase = current_user.purchases.new(purchase_params)
-
-    if @purchase.save
-      redirect_to group_purchases_path(params[:group_id]), success: 'Purchase was successfully created!'
+    @purchase = current_user.purchases.new(purchase_params.except(:group_ids))
+    @groups = Group.where(id: purchase_params[:group_ids])
+  
+    if @groups.empty?
+      flash.now[:error] = "You must choose at least one category!"
+      render :new
     else
-      @groups = current_user.groups
-      render :new, alert: 'Failed to create purchase!'
+      ActiveRecord::Base.transaction do
+        if @purchase.save
+          @groups.each { |group| group.purchases << @purchase }
+          flash[:success] = "Transaction was created and added to #{@groups.length} groups!"
+          redirect_to group_purchases_path(params[:group_id])
+        else
+          flash.now[:error] = @purchase.errors.full_messages.to_sentence
+          render :new
+          raise ActiveRecord::Rollback
+        end
+      end
     end
   end
+  
 
   def edit; end
 
   def update; end
 
   def destroy
-    @purchase = Purchase.find_by(id: params[:id])
-
-    if @purchase.nil?
-      flash[:error] = 'Transaction not found or already deleted.'
-    else
+    @purchase = Purchase.find_by_id(params[:id])
+  
+    if @purchase
       @purchase.destroy
-      flash[:success] = 'Transaction has been successfully deleted!'
+      flash[:success] = 'Transaction was successfully deleted!'
+    else
+      flash[:error] = 'Transaction not found!'
     end
-
+  
     redirect_to group_purchases_url(params[:group_id])
   end
+  
 
   def purchase_params
-    params.require(:purchase).permit(:name, :amount, group_purchases_attributes: :group_id)
+    params.require(:purchase).permit(:name, :amount, group_ids: [])
   end
 end
